3e1c6b84bebcb45d959c9a2badace6e69ecdbcab
pjlantz <patrik@pjlantz.com>
2010-08-06 14:06:34 +0200

    updated gitignore file

--- src/webdb/settings.py
+++ src/webdb/settings.py
@@ -12,10 +12,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'hale',                      # Or path to database file if using sqlite3.
-        'USER': 'peppe',                      # Not used with sqlite3.
-        'PASSWORD': '99user99',                  # Not used with sqlite3.
-        'HOST': 'pjlantz.com',                      # Set to empty string for localhost. Not used with sqlite3.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
